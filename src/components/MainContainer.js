c2aeddf46264b84a9f0d8f749573b38ba450eb4c
unknown <Ravishankar16>
2024-02-12 16:14:18 +0530

    removed unwanted comments

--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -3,7 +3,7 @@ import QuestionList from './QuestionList'
 // import QuestionListCopy2 from './QuestionListCopy2'
 const MainContainer = () => {
   return (
-    <div className='col-span-11'>
+    <div className='col-span-11 w-full'>
         <QuestionList/>
     </div>
   )
